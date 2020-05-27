0a24bf79da9ae866917fc50eb8a17184a82568dd
Zhou-Shu <54055010+Zhou-Shu@users.noreply.github.com>
2020-05-27 21:17:03 +0800

    Update Functions.r

--- Functions.r
+++ Functions.r
@@ -1,6 +1,8 @@
 library(expm)
 
-limit_law <- function(p) {
+# MC's limit law (if exist), stationary law and mean occupation law
+# p is transient probability
+MC_limit_law <- function(p) {
     n_row <- nrow(p)
     I <- diag(1, n_row)
     c <- I - p
@@ -9,8 +11,8 @@ limit_law <- function(p) {
     return(pi[n_row, ])
 }
 
-
-mean_occupation_time <- function(p, n) {
+# p = transient probability, n = transient step
+MC_mean_occupation_time <- function(p, n) {
     x <- p %^% 0
     for (i in c(1:n)) {
         x <- x + p %^% i
@@ -18,7 +20,8 @@ mean_occupation_time <- function(p, n) {
     return(x)
 }
 
-hitting_time <- function(p, n) {
+# p = transient probability, n = target status
+MC_hitting_time <- function(p, n) {
     n_row <- nrow(p) - 1
     I <- diag(1, n_row)
 
@@ -28,7 +31,16 @@ hitting_time <- function(p, n) {
     return(h %*% one)
 }
 
-transient_probability <- function(p_hat, rate, tm, error) {
+
+# get one-step transition probability of a MP
+# Q = transition rate matrix, r = a upper bound of rate
+P_hat <- function(Q,r) {
+    return(diag(nrow(Q)) + Q / r)
+}
+
+# Getting MP's transient probability by using P hat
+# p_hat = one-step transition probability, rate = P hat's rate, tm = time, error = the fitting error
+transient_probability_by_Phat <- function(p_hat, rate, tm, error) {
     nstates <- nrow(p_hat)
     Pt <- diag(nstates)
     p_hat_n <- p_hat
@@ -43,7 +55,15 @@ transient_probability <- function(p_hat, rate, tm, error) {
     return(Pt)
 }
 
+# Getting MP's transient probability by using Q (rate matrix)
+# Q = transition rate matrix, t = time
+transition_probability_by_Q <- function(t, Q){
+ Pt <- expm(t*Q)
+ return(Pt)
+ }
 
+# Get MP's mean occupation time
+# p_hat =  one-step transition probability, r = p hat's rate, t = time
 MP_mean_occupation_time<- function(p_hat,r,t)
 {
     Mt<-diag(nrow(p_hat))
@@ -53,15 +73,25 @@ MP_mean_occupation_time<- function(p_hat,r,t)
     return(r^(-1)*Mt)
 }
 
-# MOL=mean occupation law and c=cost which is a col vector
-Cost_modle<-function(MOL,c)
-{
-    return(MOL%*%c)
-}
-
+# j = target status, Q = transition rate martix of MP
 MP_hitting_time <- function(j, Q) {
     Qj <- Q[-j, -j]
     inv_Qj <- solve(Qj)
-    h_j <- rowSums(inv_Qj)
+    h_j <- -rowSums(inv_Qj)
     return(h_j)
+}
+
+# Q = transition rate martix of MP
+MP_limit_law <- function (Q)
+{
+    n_row = nrow(Q)
+    Q[, n_row] = matrix(array(1, n_row), nrow = n_row, ncol = 1, byrow = T)
+    Pi <- solve(Q) 
+    return(Pi[n_row,])
+
+}
+
+# MOL=mean occupation law or mean occupation time for MP or MC, c=cost which is a col vector
+Cost_modle <- function(MOL, c) {
+    return(MOL %*% c)
 }
